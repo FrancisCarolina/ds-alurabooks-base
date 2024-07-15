4f311c87a3aec29bd0e586d39e4ddc4fbd4df189
Francis Lovo <francis.carolinalovo@gmail.com>
2024-07-15 13:58:51 -0300

    botao secundario

--- stories/AbBotao.stories.tsx
+++ stories/AbBotao.stories.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import {AbBotao} from '../src'
+import {AbBotao, AbBotaoProps} from '../src'
 
 import {ComponentMeta, ComponentStory} from '@storybook/react'
 
@@ -9,6 +9,17 @@ export default {
     component: AbBotao
 } as ComponentMeta<typeof AbBotao>
 
-const Template: ComponentStory<typeof AbBotao> = () => <AbBotao/>
+const Template: ComponentStory<typeof AbBotao> = (args) => <AbBotao {...args}/>
 
 export const Primario = Template.bind({})
+
+Primario.args = {
+     texto: "Botão Primário"
+} as AbBotaoProps;
+
+export const Secundario = Template.bind({})
+
+Secundario.args = {
+    texto: "Botão Secundário",
+    tipo: 'secundario',
+} as AbBotaoProps;
